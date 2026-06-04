7f5f4c6ed30ff46a2e25acc41e89aab887e2efe0
Mambizah <ferytail360@gmail.com>
2026-06-04 14:22:13 +0000

    commit

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -5,11 +5,13 @@ const BASE_URL = API_BASE.endsWith('/') ? API_BASE : `${API_BASE}/`
 
 export async function uploadMultipart(path, formData) {
   const token = localStorage.getItem('taoman_admin_token')
+  const headers = {}
+  if (token) headers.Authorization = `Bearer ${token}`
   const res = await axios.post(`${BASE_URL}${path.replace(/^\//, '')}`, formData, {
-    headers: {
-      Authorization: token ? `Bearer ${token}` : undefined,
-    },
+    headers,
     timeout: 120000,
+    maxBodyLength: Infinity,
+    maxContentLength: Infinity,
   })
   return res.data
 }
